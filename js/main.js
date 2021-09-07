5420f1aea5f4d2d2c80ec6bae71cece9286081d1
TDrake38 <thomasdrake_38@hotmail.com>
2021-09-07 17:05:01 -0230

    got the pin moving and changed the pin locations

--- js/main.js
+++ js/main.js
@@ -3,113 +3,113 @@
 const locations = [
     {
         name: "Summit",
-        y: 164,
-        x: 255,
+        top: 150,
+        left: 252,
     },
     {
         name: "Military Base",
-        y: 23,
-        x: 170,
+        top: 147,
+        left: 474,
     },
     {
         name: "Salt Mine",
-        top: 45,
-        left: 235,
+        top: 198,
+        left: 633,
     },
     {
         name: "Array",
-        top: 100,
-        left: 220,
+        top: 339,
+        left: 599,
     },
     {
         name: "TV Station",
-        top: 110,
-        left: 190,
+        top: 366,
+        left: 521,
     },
     {
         name: "Air Port",
-        top: 105,
-        left: 90,
+        top: 351,
+        left: 273,
     },
     {
         name: "Super Store",
-        top: 125,
-        left: 115,
+        top: 399,
+        left: 329,
     },
     {
         name: "Factory",
-        top: 135,
-        left: 145,
+        top: 428,
+        left: 399,
     },
     {
         name: "Storage Town",
-        top: 135,
-        left: 80,
+        top: 428,
+        left: 246,
     },
     {
         name: "Stadium",
-        top: 145,
-        left: 228,
+        top: 449,
+        left: 612,
     },
     {
         name: "Lumber",
-        top: 143,
-        left: 285,
+        top: 449,
+        left: 756,
     },
     {
         name: "Hospital",
-        top: 177,
-        left: 165,
+        top: 531,
+        left: 458,
     },
     {
-        name: "Bone Yard",
-        top: 177,
-        left: 77,
+        name: "Boneyard",
+        top: 530,
+        left: 236,
     },
     {
         name: "Train Station",
-        top: 188,
-        left: 120,
+        top: 560,
+        left: 344,
     },
     {
         name: "Downtown",
-        top: 190,
-        left: 212,
+        top: 566,
+        left: 576,
     },
     {
         name: "Farmland",
-        top: 185,
-        left: 275,
+        top: 557,
+        left: 734,
     },
     {
-        name: "Prome East",
-        top: 208,
-        left: 155,
+        name: "Promenade East",
+        top: 614,
+        left: 431,
     },
     {
-        name: "Prom West",
-        top: 218,
-        left: 93,
+        name: "Promenade West",
+        top: 638,
+        left: 281,
     },
     {
         name: "Hills",
-        top: 242,
-        left: 120,
+        top: 699,
+        left: 342,
     },
     {
         name: "Park",
-        top: 232,
-        left: 201,
+        top: 675,
+        left: 548,
     },
     {
         name: "Port",
-        top: 225,
-        left: 243,
+        top: 653,
+        left: 657,
     },
     {
         name: "Prison",
-        y: 256,
-        k: 285,
+        top: 732,
+        left: 756,
     },
 ]
 
@@ -124,9 +124,12 @@ const locations = [
 const pin = document.getElementById("redDot")
 const result = document.getElementById("result1");
 const jump = document.getElementById("sendIt");
+const map = document.getElementById("map");
 const list = document.getElementById("dropList");
 let li = document.createElement("li");
 
+const IMAGE_WIDTH = 941;
+const IMAGE_HEIGHT = 845;
 
 // When you click the button it makes the random location name come up in HTML
 jump.addEventListener("click", () => {
@@ -139,10 +142,30 @@ jump.addEventListener("click", () => {
 
     liNode.appendChild(textNode);
     list.appendChild(liNode);
-    
-    var x = random(0,944)
-    var y = random(0,846)
-    //pin.
+
+
+    const { x, y, width, height } = map.getBoundingClientRect();
+
+    const scaleW = width / IMAGE_WIDTH;
+    const scaleH = height / IMAGE_HEIGHT;
+
+    console.log(scaleW, scaleH)
+
+    pin.style.top = `${(dz.top * scaleH) + y - (pin.offsetHeight)}px`;
+    pin.style.left = `${(dz.left * scaleW) + x - (pin.offsetWidth / 2)}px`;
 });
 
+// When you click the button it makes the random location name come up in HTML
+//jump.addEventListener("click", () => {
+    //let dz = locations[Math.floor(Math.random()*locations.length)];
+    //result1.innerText = dz.name;
 
+    //let dzLocation = dz.name
+    //let liNode = document.createElement("li");
+    //let textNode = document.createTextNode(dzLocation);
+
+    //liNode.appendChild(textNode);
+    //list.appendChild(liNode);
+    
+    //var x = random(0,944)
+    //var y = random(0,846)
