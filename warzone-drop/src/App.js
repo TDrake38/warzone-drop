8fa671018a0e564e45037ebf39755d09b06360d3
TDrake38 <thomasdrake_38@hotmail.com>
2022-01-09 19:31:37 -0330

    added the Caldera Map and created a home component

--- warzone-drop/src/App.js
+++ warzone-drop/src/App.js
@@ -1,10 +1,19 @@
+
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <BrowserRouter>
+        <Container>
+            <Route path="/" exact component={Home}/>
+            <Route render={() => <div>404</div>} />
+        </Container>
+      </BrowserRouter>
+    </>
   );
 }
 
