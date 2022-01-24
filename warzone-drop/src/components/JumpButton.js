87e07c497810acb64447720b887db5e62094c1dc
TDrake38 <thomasdrake_38@hotmail.com>
2022-01-24 00:13:19 -0330

    got the buttone to console log the random location but it prints 2 locations

--- warzone-drop/src/components/JumpButton.js
+++ warzone-drop/src/components/JumpButton.js
@@ -4,7 +4,7 @@ import styles from './JumpButton.module.css'
 
 function JumpButton () {
 
-    /*const locations = [
+    const locations = [
         {
             "name": "DOCKS",
             "top": 644,
@@ -81,41 +81,42 @@ function JumpButton () {
             "left": 802,
         }
     ]
+
     // -- This console logs the random location 
-    //console.log(dz)
+    
     const pin = document.getElementById("redDot")
     const result = document.getElementById("result1");
     const jump = document.getElementById("sendIt");
     const map = document.getElementById("map");
-    const list = document.getElementById("dropList");
+    // const list = document.getElementById("dropList");
     let li = document.createElement("li");
 
     const IMAGE_WIDTH = 1200;
     const IMAGE_HEIGHT = 675;
 
-    // When you click the button it makes the random location name come up in HTML
-    jump.addEventListener("click", () => {
-        let dz = locations[Math.floor(Math.random()*locations.length)];
-        result1.innerText = dz.name;
+    // // When you click the button it makes the random location name come up in HTML
+     jump.addEventListener("click", () => {
+         let dz = locations[Math.floor(Math.random()*locations.length)];
+    //     result1.innerText = dz.name;
 
-        let dzLocation = dz.name
-        let liNode = document.createElement("li");
-        let textNode = document.createTextNode(dzLocation);
+    //     let dzLocation = dz.name
+    //     let liNode = document.createElement("li");
+    //     let textNode = document.createTextNode(dzLocation);
 
-        liNode.appendChild(textNode);
-        list.appendChild(liNode);
+    //     liNode.appendChild(textNode);
+    //     list.appendChild(liNode);
 
 
-        const { x, y, width, height } = map.getBoundingClientRect();
+         //const { x, y, width, height } = map.getBoundingClientRect();
 
-        const scaleW = width / IMAGE_WIDTH;
-        const scaleH = height / IMAGE_HEIGHT;
+         //const scaleW = width / IMAGE_WIDTH;
+         //const scaleH = height / IMAGE_HEIGHT;
 
-        console.log(scaleW, scaleH)
-
-        pin.style.top = `${(dz.top * scaleH) + y - (pin.offsetHeight)}px`;
-        pin.style.left = `${(dz.left * scaleW) + x - (pin.offsetWidth / 2)}px`;
-    });*/
+        //console.log(scaleW, scaleH)
+        console.log(dz)
+    //     pin.style.top = `${(dz.top * scaleH) + y - (pin.offsetHeight)}px`;
+    //     pin.style.left = `${(dz.left * scaleW) + x - (pin.offsetWidth / 2)}px`;
+    });
     return(
         <>
             <Button variant="danger" className={styles.jump} id="sendIt">Jump</Button>
