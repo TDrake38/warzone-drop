d12319ad474a629be136a578cdd9f3c0719e2da0
TDrake38 <thomasdrake_38@hotmail.com>
2022-02-02 18:53:02 -0330

    got the result to shop up.

--- warzone-drop/src/components/JumpButton.js
+++ warzone-drop/src/components/JumpButton.js
@@ -82,40 +82,40 @@ function JumpButton () {
         }
     ]
 
-    // -- This console logs the random location 
+    // // -- This console logs the random location 
     
     const pin = document.getElementById("redDot")
     const result = document.getElementById("result1");
     const jump = document.getElementById("sendIt");
     const map = document.getElementById("map");
-    // const list = document.getElementById("dropList");
+    //const list = document.getElementById("dropList");
     let li = document.createElement("li");
 
     const IMAGE_WIDTH = 1200;
     const IMAGE_HEIGHT = 675;
 
-    // // When you click the button it makes the random location name come up in HTML
-     jump.addEventListener("click", () => {
-         let dz = locations[Math.floor(Math.random()*locations.length)];
-    //     result1.innerText = dz.name;
+    // // // When you click the button it makes the random location name come up in HTML
+    jump.addEventListener("click", () => {
+        let dz = locations[Math.floor(Math.random()*locations.length)];
+        result.innerText = dz.name;
 
-    //     let dzLocation = dz.name
-    //     let liNode = document.createElement("li");
-    //     let textNode = document.createTextNode(dzLocation);
+        let dzLocation = dz.name
+        let liNode = document.createElement("li");
+        let textNode = document.createTextNode(dzLocation);
 
-    //     liNode.appendChild(textNode);
-    //     list.appendChild(liNode);
+        // liNode.appendChild(textNode);
+        // list.appendChild(liNode);
 
 
-         //const { x, y, width, height } = map.getBoundingClientRect();
+        const { x, y, width, height } = map.getBoundingClientRect();
 
-         //const scaleW = width / IMAGE_WIDTH;
-         //const scaleH = height / IMAGE_HEIGHT;
+        const scaleW = width / IMAGE_WIDTH;
+        const scaleH = height / IMAGE_HEIGHT;
 
-        //console.log(scaleW, scaleH)
+        // console.log(scaleW, scaleH)
         console.log(dz)
-    //     pin.style.top = `${(dz.top * scaleH) + y - (pin.offsetHeight)}px`;
-    //     pin.style.left = `${(dz.left * scaleW) + x - (pin.offsetWidth / 2)}px`;
+        // pin.style.top = `${(dz.top * scaleH) + y - (pin.offsetHeight)}px`;
+        // pin.style.left = `${(dz.left * scaleW) + x - (pin.offsetWidth / 2)}px`;
     });
     return(
         <>
